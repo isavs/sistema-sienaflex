a85e1e9c7657352a3bb7965168aab78d474c85cf
Isabela Santos <isabela.vieir@hotmail.com>
2019-11-12 17:28:45 -0300

    Atributos char* substituidos por string

--- Funcionario.h
+++ Funcionario.h
@@ -4,23 +4,14 @@
 class Funcionario: public Pessoa
 {
 private:
-<<<<<<< HEAD
     static int quantidadeFuncionarios;
     int id;
-=======
-    static int id;
->>>>>>> 81234df6b28e339c95bf6fbeeb525e10a832103d
     double salario;
 
 public:
     Funcionario();
-<<<<<<< HEAD
     Funcionario(string nome);
     Funcionario(string nome, string cpf, string telefone, Endereco endereco, double salario);
-=======
-    Funcionario(char* nome);
-    Funcionario(int id, char* nome, char* cpf, char* telefone, Endereco endereco, double salario);
->>>>>>> 81234df6b28e339c95bf6fbeeb525e10a832103d
     ~Funcionario();
     const int getId();
     const double getSalario();
